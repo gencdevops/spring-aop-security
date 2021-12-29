f4eb9367c3e58d6da9824a4f4bc338c54c8df02f
EXT02D47972 <taner.genc@consultant.turkcell.com.tr>
2021-12-29 15:32:52 +0300

    Initial commit

---
+++ src/main/java/com/example/springaopsecurity/SpringAopSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaopsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopSecurityApplication.class, args);
+    }
+
+}
