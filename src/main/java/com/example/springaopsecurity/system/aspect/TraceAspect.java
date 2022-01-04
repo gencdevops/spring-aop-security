516fd74e0708ab47af4bc4effc0b5deb3f1946d5
EXT02D47972 <taner.genc@consultant.turkcell.com.tr>
2022-01-04 17:51:23 +0300

    Trace Aspect added

---
+++ src/main/java/com/example/springaopsecurity/system/aspect/TraceAspect.java
@@ -0,0 +1,16 @@
+package com.example.springaopsecurity.system.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class TraceAspect {
+
+    @Before("execution(* com.example.springaopsecurity.module.service.*.* (..))")
+    public void coming(JoinPoint joinPoint) {
+        System.out.println("before execution jointPoint");
+    }
+}
